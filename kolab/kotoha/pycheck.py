feb0bb5805d942249d0d70a125225e1815c5092e
Yuka Akinobu <sx2_vn_yuka@outlook.jp>
2021-10-07 14:58:26 +0900

    updated

--- kolab/kotoha/pycheck.py
+++ kolab/kotoha/pycheck.py
@@ -10,8 +10,9 @@ def check_syntax(s: str):
     Python の構文的に正しいかどうか
     """
     tree = parser(s.strip())
-    #print(repr(tree))
-    return not tree.isSyntaxError()
+    # print(repr(tree))
+    # return not tree.isSyntaxError()
+    return tree.isSyntaxError()
 
 def fix(tree):
     a = [tree.epos_]
@@ -31,7 +32,25 @@ def filter_syntax(s: str):
     return str(fix(tree))
 
 
+def main(argv):
+    err_sum = 0
+    line_sum = 0
+
+    if len(argv) == 0:
+        pass
+    else:
+        filename = argv[0]
+        with open(filename) as f:
+            for line in f:
+                line_sum += 1
+                err_sum += check_syntax(line)
+
+    print(f'Error rate : {err_sum}/{line_sum} ({err_sum / line_sum})')
+
+
 if __name__ == '__main__':
-    print(check_syntax(" df . groupby ( [ 'month' , 'period' ] ) [ 'sales' ] . sum (  "))
-    print(filter_syntax(" df . groupby ( [ 'month' , 'period' ] ) [ 'sales'  . sum (  "))
+    main(sys.argv[1:])
+    # print(check_syntax(" df . head ( ) "))
+    # print(check_syntax(" df . groupby ( [ 'month' , 'period' ] ) [ 'sales' ] . sum ( "))
+    # print(filter_syntax(" df . groupby ( [ 'month' , 'period' ] ) [ 'sales'  . sum (  "))
 
