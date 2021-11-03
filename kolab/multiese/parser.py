4ed914beb3808cf92529176ddb95505f51486017
kkuramitsu <kkuramitsu@gmail.com>
2021-11-03 20:30:37 +0900

    fix: add parser

--- kolab/multiese/parser.py
+++ kolab/multiese/parser.py
@@ -1,8 +1,10 @@
 import pegtree as pg
 from pegtree import ParseTree
 from pegtree.visitor import ParseTreeVisitor
+import tree as ntree
+import pprint
 
-peg = pg.grammar('multiese')
+peg = pg.grammar('multiese.pegtree')
 parser = pg.generate(peg)
 
 
@@ -21,7 +23,57 @@ class MultieseParser(ParseTreeVisitor):
     def __init__(self):
         ParseTreeVisitor.__init__(self)
 
-    # [#Expression e]
+    def parse(self, s: str):
+        tree = parser(s)
+        node = self.visit(tree)
+        return node
+
+    def acceptChunk(self, tree: ParseTree):
+        s = str(tree)
+        node = ntree.parse(s)
+        return ntree.系列(*node.flatten()).simplify()
+
+    def acceptSeq(self, tree: ParseTree):
+        ns = []
+        # print(repr(tree))
+        for t in tree:
+            node = self.visit(t)
+            node.flatten(ns)
+        return ntree.系列(*ns)
+
+    def acceptBlock(self, tree: ParseTree):
+        # print(repr(tree))　(1+2)*3
+        return ntree.グループ(self.visit(tree[0]))
+
+    def acceptChoice(self, tree: ParseTree):
+        ns = []
+        for t in tree:
+            ns.append(self.visit(t))
+        return ntree.Choice(ns)
 
     def acceptExpression(self, tree: ParseTree):
-        return self.visit(tree[0])
+        s = str(fix(tree))
+        return ntree.コード(s)
+
+    def acceptAnnotation(self, tree: ParseTree):
+        name = str(tree[0])  # アノテーション種類
+        ns = [self.visit(t) for t in tree[1:]]
+        return ntree.annotation(name, ns)
+
+
+mult = MultieseParser()
+
+
+def test_for_nobu(s):
+    print(s)
+    print('=>', repr(mult.parse(s)))
+    print()
+
+
+test_for_nobu('データフレームdfを降順にソートする')
+test_for_nobu('望遠鏡で泳ぐ子犬を見た')
+test_for_nobu('望遠鏡で{泳ぐ子犬}を見た')
+test_for_nobu('望遠鏡で[子犬|とうきび]を見た')
+test_for_nobu('@type(df, データフレーム)において、望遠鏡で子犬を見た')
+test_for_nobu('@type(df)の先頭を見る')
+# test_for_nobu('望遠鏡で{子犬が泳ぐ}様子を見たら、math.pi+1を実行する')
--- kolab/multiese/tree.py
+++ kolab/multiese/tree.py
@@ -4,17 +4,18 @@ import sys
 janome = Tokenizer()
 
 OPTION = {
-#     '--random': True,
-#     '--single': False, # ひとつしか選ばない (DAはオフ)
-    '--order': False, # 順序も入れ替える
-#     '--short': False, # 短い類義語を選ぶ
-#     '--pyfirst': False, #Pythonを先に出力する (yk使用)
-#     '--change-subject': 0.0, # 助詞の「が」をランダムに「は」に変える
-#     '--drop': 0.0, # パラメータをドロップする
-#     '--partial': False, #不完全なコードでも出力する
+    #     '--random': True,
+    #     '--single': False, # ひとつしか選ばない (DAはオフ)
+    '--order': False,  # 順序も入れ替える
+    #     '--short': False, # 短い類義語を選ぶ
+    #     '--pyfirst': False, #Pythonを先に出力する (yk使用)
+    #     '--change-subject': 0.0, # 助詞の「が」をランダムに「は」に変える
+    #     '--drop': 0.0, # パラメータをドロップする
+    #     '--partial': False, #不完全なコードでも出力する
 }
 
-class ノード(object):  # 抽象的なクラス
+
+class ノード(object):  # 抽象的なトップクラス
 
     def emit(self, out):
         pass
@@ -24,31 +25,19 @@ class ノード(object):  # 抽象的なクラス
         self.emit(out)
         return ''.join(out)
 
-    def __repr__(self):
-        return f"<{self.__class__.__name__}> " + self.stringfy()
+    def flatten(self, ns=None):
+        if ns is None:
+            ns = []
+        ns.append(self)
+        return ns
 
-class 文(ノード):
-    ws: list  # 文節のリスト
+    def simplify(self):
+        """シンプルにしてノードを返す
+        """
+        return self
 
-    def __init__(self, *ws):
-        self.ws = ws
 
-    def emit(self, out):
-        for w in self.ws:
-            out.append('/')
-            w.emit(out)
-
-class 文節(ノード):
-    ws: list
-
-    def __init__(self, *ws):
-        self.ws = ws
-
-    def emit(self, out):
-        for w in self.ws:
-            w.emit(out)
-
-class 名詞(ノード):
+class 字句(ノード):  # 抽象的な字句
     w: str
 
     def __init__(self, w):
@@ -58,89 +47,166 @@ class 名詞(ノード):
         # 類義語に置き換える処理を書けばよい
         out.append(self.w)
 
-class 助詞(ノード):
-    w: str
+    def __repr__(self):  # repr
+        return f"[{self.__class__.__name__} {repr(self.w)}]"
 
-    def __init__(self, w):
-        self.w = w
 
-    def emit(self, out):
-        out.append(self.w)
+class 系列(ノード):  # 系列
+    ws: list  # 系列のリスト
 
-class 助動詞(ノード):
-    w: str
-
-    def __init__(self, w):
-        self.w = w
+    def __init__(self, *ws):
+        self.ws = ws
 
     def emit(self, out):
-        out.append(self.w)
+        for w in self.ws:
+            w.emit(out)
 
-class 形容詞(ノード):
-    w: str
+    def flatten(self, ns=None):
+        if ns is None:
+            ns = []
+        for w in self.ws:
+            w.flatten(ns)
+        return ns
 
-    def __init__(self, w):
-        self.w = w
+    def simplify(self):
+        if len(self.ws) == 1:
+            return self.ws[0]
+        return self
 
-    def emit(self, out):
-        out.append(self.w)
+    def __repr__(self):
+        s = ' '.join(map(repr, self.ws))
+        return f"[{self.__class__.__name__} {s}]"
 
-class 連体詞(ノード):
-    w: str
 
-    def __init__(self, w):
-        self.w = w
+class グループ(ノード):  # 外からみると、字句として扱えるが、中には系列が入っている
+    node: str
 
-    def emit(self, out):
-        out.append(self.w)
-
-class 副詞(ノード):
-    w: str
-
-    def __init__(self, w):
-        self.w = w
-
-    def emit(self, out):
-        out.append(self.w)
-
-class 接続詞(ノード):
-    w: str
-
-    def __init__(self, w):
-        self.w = w
+    def __init__(self, node):
+        self.node = node
 
     def emit(self, out):
-        out.append(self.w)
+        self.node.emit(out)
+
+    def __repr__(self):  # repr
+        return f"[{self.__class__.__name__} {repr(self.node)}]"
 
 
-class 動詞(ノード):
-    w: str
+class Choice(ノード):  # 系列が入っている字句として扱えるが、中には系列が入っている
+    nodes: list
 
-    def __init__(self, w):
-        self.w = w
-    
+    def __init__(self, nodes):
+        self.nodes = nodes
+
     def emit(self, out):
-        out.append(self.w)
+        pass  # がんばれ
+        # nodes のどれかを選べばよい
 
-class 記号(ノード):
-    w: str
+    def __repr__(self):  # repr
+        s = ' '.join(map(repr, self.nodes))
+        return f"[{self.__class__.__name__} {s}]"
 
-    def __init__(self, w):
-        self.w = w
-    
+
+# アノテーション
+
+
+class Annotation(ノード):  # 本来ならアノテーションごとに作った方がよい
+    name: str
+    nodes: list
+
+    def __init__(self, name, nodes):
+        self.name = name
+        self.nodes = nodes
+
     def emit(self, out):
-        out.append(self.w)
+        pass  # がんばれ
+
+    def __repr__(self):  # repr
+        s = ' '.join(map(repr, self.nodes))
+        return f"[{self.__class__.__name__} {self.name} {s}]"
+
+
+class 型情報(ノード):  # 本来ならアノテーションごとに作った方がよい
+    name: str  # 変数名
+    desc: str  # 型情報
+
+    def __init__(self, name, desc):
+        self.name = name
+        self.desc = desc
+
+    def emit(self, out):
+        pass  # がんばれ
+
+    def __repr__(self):  # repr
+        return f"[{self.__class__.__name__} {self.name} {self.desc}]"
+
+
+def annotation(name: str, nodes):
+    if name == 'type':
+        if len(nodes) == 1:
+            return 型情報(nodes[0].stringfy(), '')
+        return 型情報(nodes[0].stringfy(), nodes[1].stringfy())
+    return Annotation(name, nodes)
+
+
+class 文(系列):
+    def emit(self, out):
+        # 変更するところだけ定義する
+        for w in self.ws:
+            w.emit(out)
+
+
+class 文節(系列):
+    pass
+
+
+# 末端(字句)
+
+class 名詞(字句):
+    pass
+
+
+class 助詞(字句):
+    pass
+
+
+class 助動詞(字句):
+    pass
+
+
+class 形容詞(字句):
+    pass
+
+
+class 連体詞(字句):
+    pass
+
+
+class 副詞(字句):
+    pass
+
+
+class 接続詞(字句):
+    pass
+
+
+class 動詞(字句):
+    pass
+
+
+class コード(字句):
+    pass
+
+
+class 記号(字句):
+    pass
+
 
 class 未定義(ノード):
-    w: str
-
-    def __init__(self, w):
-        self.w = w
-    
     def emit(self, out):
         out.append(f'@{self.w}')
 
-def parse(s: str):
+
+def parse(s: str) -> ノード:
     '''
     janome で解析した結果から、文オブジェクトを返す
     https://note.nkmk.me/python-janome-tutorial/
@@ -148,11 +214,11 @@ def parse(s: str):
     buf_pos = []
     buf_phrase = []
 
-
     wakati = [token.surface for token in janome.tokenize(s)]   # 分かち書きのリスト
     # wakati = [token.base_form for token in janome.tokenize(s)]   # 基本形 (標準形) のリスト
 
-    pos = [token.part_of_speech.split(',')[0] for token in janome.tokenize(s)]   # 品詞のリスト
+    pos = [token.part_of_speech.split(',')[0]
+           for token in janome.tokenize(s)]   # 品詞のリスト
     pos2 = [token.part_of_speech.split(',')[1] for token in janome.tokenize(s)]
     # pos3 = [token.part_of_speech.split(',')[2] for token in janome.tokenize(s)]
 
@@ -205,7 +271,7 @@ def parse(s: str):
             x = 文節(*buf_pos)
             buf_phrase.append(x)
             buf_pos = []
-            
+
         elif pos[idx] == '記号':
             if pos2[idx] == '空白' or pos2[idx] == '句点':
                 pass
@@ -238,6 +304,7 @@ def parse(s: str):
 
     return s
 
+
 def read_txt(input_filename):
     '''
     テキストファイルを1行ずつ読み込んで
@@ -248,6 +315,7 @@ def read_txt(input_filename):
             s = parse(line.strip())
             print(s.stringfy())
 
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         for filename in sys.argv[1:]:
@@ -255,7 +323,8 @@ if __name__ == '__main__':
                 if '=' not in filename:
                     filename += '=True'
                 key, value = filename.split('=')
-                OPTION[key] = int(value) if value.isdigit() else value == 'True'
+                OPTION[key] = int(
+                    value) if value.isdigit() else value == 'True'
                 continue
 
             try:
@@ -267,13 +336,12 @@ if __name__ == '__main__':
         # s2 = parse('データフレームdfを逆順にソートして、df2とする')
 
         s = parse('隣の客はよく柿食う客だ')
+        print(repr(s), s.stringfy())
 
         s = parse('その写真は美しい')
-        s2 = parse('美しい写真を眺める')
+        s2 = parse('美しい写真を眺める').simplify()
 
-        s = parse('大きな犬')
+        s = parse('大きな犬').simplify()
 
-
-        print(s.stringfy())
-        print(s2.stringfy())
-
+        print(repr(s), s.stringfy())
+        print(repr(s2), s2.stringfy())
