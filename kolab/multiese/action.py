e9786d1a0a84767d40bc8337ce456ea19bc1ec93
Kimio Kuramitsu <kkuramitsu@gmail.com>
2021-11-25 16:14:59 +0900

    added @@noun

--- kolab/multiese/action.py
+++ kolab/multiese/action.py
@@ -38,11 +38,16 @@ def perform_not(pairs, option):
         pairs_not.append((sentence_not, code_not))
     return pairs_not
 
-def optional_choice(option, s, seed=0):
+def alt(s, random_seed):
     choice = s.split('|') if isinstance(s, str) else s
+    return choice[random_seed % len(choice)]
+
+def random_seed(option, seed=0):
     r = option.get('random', random.random())
-    max = len(choice)
-    return choice[int(max * r + seed) % max]
+    return int(seed + 100 * r)
+
+def optional_choice(option, s, seed=0):
+    return alt(s, random_seed(option, seed))
 
 def perform_if(pairs, option):
     ## pairs.extends(perform_not(pairs, option)) # @@not を加える
@@ -63,6 +68,95 @@ def perform_while(pairs, option):
         sentence_while = sentence + 'の間'
         pairs_while.append((sentence_while, code_while))
     return pairs_while
+
+def isGroup2Verb(s):
+    if s.endswith(s) and len(s)>2:
+        return s[-2] in 'けせてねれえげべ'
+    return False
+
+def transform_verb_and_then(s, random_seed):
+    if s.endswith('する'):
+        return s[:-2]+alt('して、|し、', random_seed)
+    if s.endswith('ずる'): #論ずる　
+        return s[:-2]+alt('じて、|じ、', random_seed)
+    if isGroup2Verb(s):  # グループ２ # ラ行と区別が難しい
+        return s[:-1]+alt('、|、|て、', random_seed)
+    if s.endswith('く'):  # 書く 
+        return s[:-1]+alt('き、|いて、', random_seed)
+    if s.endswith('す'):  # 探す 
+        return s[:-1]+alt('し、|して、', random_seed)
+    if s.endswith('つ'):  # 勝つ 
+        return s[:-1]+alt('ち、|って、', random_seed)
+    if s.endswith('ぬ'):  # 死ぬ 
+        return s[:-1]+alt('に、|んて、', random_seed)
+    if s.endswith('む'):  # 読む 
+        return s[:-1]+alt('み、|んで、', random_seed)
+    if s.endswith('る'):  # 切る   ラ行
+        return s[:-1]+alt('り、|って、', random_seed)
+    if s.endswith('う'):  # 笑う 
+        return s[:-1]+alt('い、|って、', random_seed)
+    if s.endswith('ぐ'):  # 防ぐ 
+        return s[:-1]+alt('ぎ、|いで、', random_seed)
+    if s.endswith('ぶ'):  # 遊ぶ 
+        return s[:-1]+alt('び、|んで、', random_seed)
+    return None
+
+def transform_verb_and_noun(s, noun, random_seed=0):
+    if s.endswith('する'):
+        return s[:-2]+alt('した|された', random_seed) + noun
+    if s.endswith('ずる'): #論ずる　
+        return s[:-2]+alt('じた、|じられた、', random_seed) + noun
+    if isGroup2Verb(s):  # グループ２ # ラ行と区別が難しい
+        return s[:-1]+alt('た|られた', random_seed) + noun
+    if s.endswith('く'):  # 書く 
+        return s[:-1]+alt('いた、|かれた、', random_seed) + noun
+    if s.endswith('す'):  # 探す 
+        return s[:-1]+alt('した、|された、', random_seed) + noun
+    if s.endswith('つ'):  # 勝つ 
+        return s[:-1]+alt('った|たれた', random_seed) + noun
+    if s.endswith('ぬ'):  # 死ぬ 
+        return s[:-1]+alt('んだ|なれた', random_seed) + noun
+    if s.endswith('む'):  # 読む 
+        return s[:-1]+alt('んだ|まれた', random_seed) + noun
+    if s.endswith('る'):  # 切る   ラ行
+        return s[:-1]+alt('った|られた', random_seed) + noun
+    if s.endswith('う'):  # 笑う 
+        return s[:-1]+alt('った|われた、', random_seed) + noun
+    if s.endswith('ぐ'):  # 防ぐ 
+        return s[:-1]+alt('いだ|がれた', random_seed) + noun
+    if s.endswith('ぶ'):  # 遊ぶ 
+        return s[:-1]+alt('んだ|ばれた', random_seed) + noun
+    return None
+
+def perform_noun(pairs, option): # 名詞に変える
+    pairs_noun = []
+    for sentence, code in pairs:
+        sentence_and_noun = transform_verb_and_noun(sentence, '', random_seed(option))
+        if sentence_and_noun:
+            noun = optional_choice(option, option.get('action-with', '結果') + '|値|結果') 
+            pairs_noun.append((sentence_and_noun + noun, code))
+    return pairs_noun
+
+
+def perform_let(pairs, option):  # 代入文に変える
+    pairs_let = []
+    for sentence, code in pairs:
+        code_let = f'X = {code}'
+        sentence_and_then = transform_verb_and_then(sentence, random_seed(option))
+        if sentence_and_then:
+            sentence_let = sentence_and_then + 'X' + optional_choice(option, 'にする|とする|に代入する')
+            pairs_let.append((sentence_let, code_let))
+            noun = optional_choice(option, option.get('action-with', '結果'))
+            sentence = transform_verb_and_noun(sentence, noun, 0)
+        sentence_let = sentence + 'をX' + optional_choice(option, 'にする|とする|に代入する')
+        pairs_let.append((sentence_let, code_let))
+        if sentence.endswith('かどうか'):
+            ## 小さいかどうか　=> 小さいとき、Xを真とする
+            s, _ = remove_whether(sentence)
+            sentence_let = s + 'とき、Xを真' + optional_choice(option, 'にする|とする', seed=1)
+            pairs_let.append((sentence_let, code_let))
+    return pairs_let
+
 
 def perform_check(pairs, option):
     pairs_check = []
@@ -111,9 +205,23 @@ def perform_calc(pairs, option):
 def perform_filter(actions, pairs, option):
     actions = actions.split('.')   # e.g.: @@if.not
     for action in actions:
+        if 'action-with' in option:
+            del option['action-with']
+        if action.endswith(']'): # argument
+            action, argument = action[:-1].split('[')
+            option['action-with'] = argument
+        if action.endswith('?'):
+            action = action[:-1]
+            join = True
+        else:
+            join = False
         f = f'perform_{action}'
         if f in Functions:
-            pairs = Functions[f](pairs, option)
+            new_pairs = Functions[f](pairs, option)
+            if join:
+                pairs.extend(new_pairs)
+            else:
+                pairs = new_pairs
     return pairs
 
 
